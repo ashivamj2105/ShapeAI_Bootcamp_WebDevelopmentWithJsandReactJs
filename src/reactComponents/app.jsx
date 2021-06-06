3f75fe1c4cb7e3d298bb52bd6c81721ad11adb73
ashivamj2105 <54582829+ashivamj2105@users.noreply.github.com>
2021-06-06 10:41:10 +0530

    Add files via upload

---
+++ src/reactComponents/app.jsx
@@ -0,0 +1,18 @@
+import "../../public/styles.css";
+import React from "react";
+import Header from "./header";
+import Footer from "./footer";
+import Note from "./note";
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Note></Note>
+      <Note></Note>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/reactComponents/note.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript & ReactJS</h1>
+      <hr />
+      <p>
+        A 7 days bootcamp taken by renowned faculty of ShapeAI. The aim of this
+        bootcamp is teach about the basics of HTML, Javascript and working of
+        the React Framework.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
